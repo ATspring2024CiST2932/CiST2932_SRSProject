f4d30542c38e94a7e43fc938d42a1f2b8a1da604
ATspring2024CiST2932 <159198045+ATspring2024CiST2932@users.noreply.github.com>
2024-02-24 11:56:06 -0500

    24Feb

---
+++ src/main/java/com/CiST2932/SRSProject/SrsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.CiST2932.SRSProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CiST2932/SRSProject/SrsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CiST2932.SRSProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
