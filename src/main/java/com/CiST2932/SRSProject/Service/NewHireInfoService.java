f80df51afe3b2731efea958ae28c1ba5b2ce87e0
ATspring2024CiST2932 <159198045+ATspring2024CiST2932@users.noreply.github.com>
2024-03-12 16:40:41 -0400

    Update NewHireInfoService.java

--- src/main/java/com/CiST2932/SRSProject/Service/NewHireInfoService.java
+++ src/main/java/com/CiST2932/SRSProject/Service/NewHireInfoService.java
@@ -3,13 +3,11 @@
 package com.CiST2932.SRSProject.Service;
 
 import com.CiST2932.SRSProject.Domain.NewHireInfo;
-import com.CiST2932.SRSProject.Repository.NewHireInfoDto;
 import com.CiST2932.SRSProject.Repository.NewHireInfoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Service
@@ -26,10 +24,6 @@ public class NewHireInfoService {
         return newHireInfoRepository.findById(id);
     }
 
-    public List<NewHireInfo> findAllMentors() {
-        return newHireInfoRepository.findByMentor(true);
-    }     
-
     @SuppressWarnings("null")
     public NewHireInfo save(NewHireInfo newHireInfo) {
         return newHireInfoRepository.save(newHireInfo);
@@ -38,37 +32,4 @@ public class NewHireInfoService {
     public void deleteById(int id) {
         newHireInfoRepository.deleteById(id);
     }
-
-    // public List<NewHireInfo> findMenteesByMentorId(int mentorId) {
-    //     return newHireInfoRepository.findMenteesByMentorId(mentorId);
-    // }
-
-    // public NewHireInfo assignMentor(int menteeId, int mentorId) {
-    //     Optional<NewHireInfo> mentee = newHireInfoRepository.findById(menteeId);
-    //     if (mentee.isPresent()) {
-    //         NewHireInfo updatedMentee = mentee.get();
-    //         updatedMentee.setMentorId(mentorId);
-    //         return newHireInfoRepository.save(updatedMentee);
-    //     } else {
-    //         throw new NoSuchElementException("Mentee with id " + menteeId + " not found");
-    //     }
-    // }
-
-    public List<NewHireInfo> findDistinctMentors() {
-        return newHireInfoRepository.findDistinctMentors();
-    }
-
-    public List<NewHireInfoDto> findMenteesByMentorId(Integer mentorId) {
-        return newHireInfoRepository.findMenteesByMentorId(mentorId);
-    }
-    
-    public List<NewHireInfo> findAllMentees() {
-        return newHireInfoRepository.findAllMentees();
-    }
-
-    public List<NewHireInfoDto> findAllMenteesDto() {
-        return newHireInfoRepository.findAllMenteesDto();
-    }
-    
-    
 }
