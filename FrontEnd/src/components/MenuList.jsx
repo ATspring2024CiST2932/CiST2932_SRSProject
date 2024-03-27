5083ce97185dd951173d39b59a63a3a8c42cfd81
SethRamey <141290181+SethRamey@users.noreply.github.com>
2024-03-26 21:17:06 -0400

    Fixed application opening new tabs every time you click a link

--- FrontEnd/src/components/MenuList.jsx
+++ FrontEnd/src/components/MenuList.jsx
@@ -10,12 +10,12 @@ const MenuList = ({ darkTheme }) => {
     return (
         <Menu theme={darkTheme ? 'dark' : 'light'} mode="inline" className="menu-bar">
              <Menu.Item key="tasks" icon={<ProfileOutlined />}>
-        <a href="/tasks.html" target="_blank" rel="noopener noreferrer">
+        <a href="/tasks.html" target="" rel="noopener noreferrer">
           Tasks
         </a>
         </Menu.Item>
         <Menu.Item key="Employees" icon={<ProfileOutlined />}>
-        <a href="/index.html" target="_blank" rel="noopener noreferrer">
+        <a href="/index.html" target="" rel="noopener noreferrer">
           Employees
         </a>
         </Menu.Item>
