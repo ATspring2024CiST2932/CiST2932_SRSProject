9cf0f1babcfe43139ef28b531a2f8594e69bd9a3
aniyahsss <stampleyaniyah@gmail.com>
2024-04-19 15:43:05 -0400

    updated interface

--- FrontEnd/src/components/MenuList.jsx
+++ FrontEnd/src/components/MenuList.jsx
@@ -1,41 +1,32 @@
-import { ProfileOutlined, SettingOutlined } from '@ant-design/icons';
-import { Menu } from 'antd';
+import React from 'react';
+import {Menu} from 'antd';
+import { ProfileOutlined, UserOutlined, SettingOutlined } from '@ant-design/icons';
+
+
+
+
 
 const MenuList = ({ darkTheme }) => {
-  const menuItems = [
-    {
-      key: 'tasks',
-      icon: <ProfileOutlined />,
-      label: (
+    return (
+        <Menu theme={darkTheme ? 'dark' : 'light'} mode="inline" className="menu-bar">
+             <Menu.Item key="tasks" icon={<ProfileOutlined />}>
         <a href="/tasks.html" target="" rel="noopener noreferrer">
           Tasks
         </a>
-      ),
-    },
-    {
-      key: 'Employees',
-      icon: <ProfileOutlined />,
-      label: (
+        </Menu.Item>
+        <Menu.Item key="Employees" icon={<ProfileOutlined />}>
         <a href="/index.html" target="" rel="noopener noreferrer">
           Employees
         </a>
-      ),
-    },
-    {
-      key: 'settings',
-      icon: <SettingOutlined />,
-      label: 'Settings',
-    },
-  ];
+        </Menu.Item>
+            <Menu.Item key="Login" icon={<SettingOutlined />}>
+            <a href="/login.html" target="" rel="noopener noreferrer">
+              Login
+              </a>
+              </Menu.Item>
+        </Menu>
+    )
+    
+}
 
-  return (
-    <Menu
-      theme={darkTheme ? 'dark' : 'light'}
-      mode="inline"
-      className="menu-bar"
-      items={menuItems}
-    />
-  );
-};
-
-export default MenuList;
+export default MenuList
