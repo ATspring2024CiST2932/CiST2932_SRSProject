3b3604362d1ca7690f19070443b2203368e005d8
ATspring2024CiST2932 <159198045+ATspring2024CiST2932@users.noreply.github.com>
2024-04-27 12:54:47 -0400

    27Apr

--- src/main/java/com/CiST2932/SRSProject/Repository/NewHireInfoRepository.java
+++ src/main/java/com/CiST2932/SRSProject/Repository/NewHireInfoRepository.java
@@ -4,8 +4,6 @@ package com.CiST2932.SRSProject.Repository;
 
 import com.CiST2932.SRSProject.Domain.NewHireInfo;
 
-import jakarta.transaction.Transactional;
-
 import java.util.List;
 import java.util.Optional;
 
@@ -14,6 +12,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Repository
