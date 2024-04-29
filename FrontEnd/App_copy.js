17bbf16d2f210d57785104a019f6ef8efe91238c
aniyahsss <stampleyaniyah@gmail.com>
2024-04-29 00:12:03 -0400

    buttons fixed

--- FrontEnd/App_copy.js
+++ FrontEnd/App_copy.js
@@ -171,11 +171,10 @@ function fetchAllEmployees() {
           <td>${employee.employmentType}</td>
           <td>${employee.isMentor ? 'Yes' : 'No'}</td>
           <td>
-            <button class="btn btn-success" onclick="viewEmployee(${employee.employeeId})"><i class="bi bi-eye"></i></button>
-            <button class="btn btn-primary" onclick="editEmployee(${employee.employeeId})"><i class="bi bi-pencil-square"></i></button>
-            <button class="btn btn-danger" onclick="archiveEmployee(${employee.employeeId})"><i class="bi bi-trash"></i></button>
+            <button class="btn btn-success" onclick="viewEmployee(${employee.employeeId})">View</button>
+            <button class="btn btn-primary" onclick="editEmployee(${employee.employeeId})">Edit</i></button>
+            <button class="btn btn-danger" onclick="archiveEmployee(${employee.employeeId})">Delete</i></button>
           </td>
-
         `;
         employeeTableBody.appendChild(row);
       });
